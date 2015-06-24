6e947c0341ae444144e7e2d86dbeccb455b40bfd
Sergey Miroshnikov <hyperxai@gmail.com>
2015-06-24 17:09:49 +0300

    update ninja form plug

--- wp-content/plugins/ninja-forms/includes/admin/pages/ninja-forms-settings/tabs/general-settings/general-settings.php
+++ wp-content/plugins/ninja-forms/includes/admin/pages/ninja-forms-settings/tabs/general-settings/general-settings.php
@@ -1,4 +1,4 @@
-<?php
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
 add_action( 'init', 'ninja_forms_register_tab_general_settings', 9 );
 
 function ninja_forms_register_tab_general_settings(){
@@ -29,27 +29,68 @@ function ninja_forms_register_general_settings_metabox(){
 		'title' => __( 'General Settings', 'ninja-forms' ),
 		'settings' => array(
 			array(
-				'name' => 'version',
-				'type' => 'desc',
+				'name' 	=> 'version',
+				'type' 	=> 'desc',
 				'label' => __( 'Version', 'ninja-forms' ),
-				'desc' => $current_version,
+				'desc' 	=> $current_version,
 			),
 			array(
-				'name' => 'date_format',
-				'type' => 'text',
+				'name' 	=> 'date_format',
+				'type' 	=> 'text',
 				'label' => __( 'Date Format', 'ninja-forms' ),
-				'desc' => __( 'e.g. m/d/Y, d/m/Y - Tries to follow the <a href="http://www.php.net/manual/en/function.date.php" target="_blank">PHP date() function</a> specifications, but not every format is supported.', 'ninja-forms' ),
+				'desc' 	=> 'e.g. m/d/Y, d/m/Y - ' . sprintf( __( 'Tries to follow the %sPHP date() function%s specifications, but not every format is supported.', 'ninja-forms' ), '<a href="http://www.php.net/manual/en/function.date.php" target="_blank">', '</a>' ),
 			),
 			array(
-				'name' => 'currency_symbol',
-				'type' => 'text',
+				'name' 	=> 'currency_symbol',
+				'type' 	=> 'text',
 				'label' => __( 'Currency Symbol', 'ninja-forms' ),
-				'desc' => __( 'e.g. $, &pound;, &euro;', 'ninja-forms' ),
+				'desc' 	=> 'e.g. $, &pound;, &euro;',
 			),
 		),
 	);
+	ninja_forms_register_tab_metabox( $args );	
+
+	$args = array(
+		'page' => 'ninja-forms-settings',
+		'tab' => 'general_settings',
+		'slug' => 'advanced_settings',
+		'title' => __( 'Advanced Settings', 'ninja-forms' ),
+		'settings' => array(
+			array(
+				'name'	=> 'delete_on_uninstall',
+				'type'	=> 'checkbox',
+				'label'	=> __( 'Remove ALL Ninja Forms data upon uninstall?', 'ninja-forms' ),
+				'desc'	=> sprintf( __( 'If this box is checked, ALL Ninja Forms data will be removed from the database upon deletion. %sAll form and submission data will be unrecoverable.%s', 'ninja-forms' ), '<span class="nf-nuke-warning">', '</span>' ),
+			),
+			array(
+				'name'	=> 'delete_prompt',
+				'type'	=> '',
+				'display_function' => 'nf_delete_on_uninstall_prompt',
+			),
+		),
+		'state' => 'closed',
+	);
+    $args['settings'] = apply_filters( 'nf_general_settings_advanced', $args['settings'] );
+
 	ninja_forms_register_tab_metabox( $args );
 
+}
+
+function nf_delete_on_uninstall_prompt() {
+	?>
+	<div class="nf-delete-on-uninstall-prompt">
+		<?php _e( 'This setting will COMPLETELY remove anything Ninja Forms related upon plugin deletion. This includes SUBMISSIONS and FORMS. It cannot be undone.', 'ninja-forms' ); ?>
+	</div>
+	<div class="nf-delete-on-uninstall-prompt-buttons">
+		<div id="nf-admin-modal-cancel">
+            <a class="submitdelete deletion modal-close nf-delete-on-uninstall-cancel" href="#"><?php _e( 'Cancel', 'ninja-forms' ); ?></a>
+        </div>
+        <div id="nf-admin-modal-update">
+            <a class="button-primary nf-delete-on-uninstall-yes" href="#"><?php _e( 'Continue', 'ninja-forms' ); ?></a>
+        </div>
+	</div>
+
+	<?php
 }
 
 function ninja_forms_save_general_settings( $data ){
